941bcbd291fbdf3ce67aad24a86b56a74ba0ddf6
aplumly <chimpsahoy99@gmail.com>
2018-05-11 08:31:32 -0500

    wordset now consists of famous outlaws. losing winning reveals a link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,21 @@
         let boolio                  = new Array();//use this to determine if letters display
         let guesses_remaining       =12;
         let times_guessed           =0;
-        let wordset                 = new Array("marshall","bandit","holster",
-                                    "cowboy","whiskey","boot","texas","sherrif",
-                                    "hangman","noose","game","poker","colt",
-                                    "pony","cowgirl","outlaw");
+        const links                 = new Array("http://www.thewildwest.org/cowboys/wildwestoutlawsandlawmen/172-billythekid",
+                                    "https://en.wikipedia.org/wiki/Ike_Clanton",
+                                    "https://en.wikipedia.org/wiki/Butch_Cassidy","https://en.wikipedia.org/wiki/Charlie_Bowdre",
+                                    "https://www.legendsofamerica.com/tom-pickett/","https://en.wikipedia.org/wiki/Dave_Rudabaugh",
+                                    "http://www.thewildwest.org/cowboys/wildwestoutlawsandlawmen/182-tom-horn",
+                                    "https://en.wikipedia.org/wiki/Frank_McLaury","https://en.wikipedia.org/wiki/William_Brocius",
+                                    "http://www.thewildwest.org/cowboys/wildwestoutlawsandlawmen/186-wildwestoutlawjohnringo",
+                                    "http://www.thewildwest.org/cowboys/wildwestoutlawsandlawmen/177-wyattearp",
+                                    "https://www.legendsofamerica.com/we-harveylogan/",
+                                    "https://en.wikipedia.org/wiki/George_Curry_(Wild_Bunch)",
+                                    "https://en.wikipedia.org/wiki/Sundance_Kid");
+        const wordset               = new Array("billy    the    kid","ike    clanton","butch    cassidy",
+                                    "charlie    bowdre","tom    pickett","dave    rudabaugh","tom    horn","frank    mclaury",
+                                    "william     brocius","johnny    ringo","wyatt    earp","harvey    logan","george    curry",
+                                    "sundance     kid");
         let guess                   = "";
         let wins                    = 0;
         let losses                  = 0;
@@ -15,22 +26,32 @@
         let word                    = wordset[seed];
         let ws                      = "";
         let check                   = false;
-        let winsd                   = document.getElementById("wins");
-        let lossesd                 = document.getElementById("losses");
-        let guesses_leftd           = document.getElementById("guesses_left");
-        let guessed                 = document.getElementById("guessed");
-        let wordspace               = document.getElementById("wordspace");
-        let hangman_pic             = document.getElementById("hangman");
-        let message                 = document.getElementById("message");
-        let gptext                  = document.getElementById("gptext");
-        let game                    = true;
+        const winsd                 = document.getElementById("wins");
+        const lossesd               = document.getElementById("losses");
+        const guesses_leftd         = document.getElementById("guesses_left");
+        const guessed               = document.getElementById("guessed");
+        const wordspace             = document.getElementById("wordspace");
+        const hangman_pic           = document.getElementById("hangman");
+        const message               = document.getElementById("message");
+        const gptext                = document.getElementById("gptext");
+        const linkspace             = document.getElementById("linkspace");
+        let game                    = false;
         boolio.length               = word.length;
         let acceptable_keys         ="abcdefghijklmnopqrstuvwxyz ";
         let gen_purpose_var;
-       // var good_bad_ugly           = document.getElementById("music");
         var good_bad_ugly           = new Audio("assets/audio/goodbadugly.mp3"); 
         
-       
+        function showlink()
+        {
+            linkspace.innerHTML= word;
+            linkspace.href= links[seed];
+        } 
+
+        function hidelink()
+        {
+            linkspace.innerHTML = "";
+            linkspace.href= "#";
+        }
       
         function new_word()
         {   
@@ -52,7 +73,7 @@
             boolio.length = word.length;
             let a=1;
             for(let i=0;i<=word.length-1;i++)
-            {   
+            {   if(word[i]===" "){boolio[i]=true;}
                 if(boolio[i]==true || word[i]===" "){ws=ws+word[i];}
                 else{ws=ws+"_";}
             }
@@ -97,6 +118,7 @@
 
         function lose()
         {   
+            showlink();
             ws="";
             guessed_letters.length  = 0;
             boolio.length           = 0;
@@ -108,7 +130,7 @@
             times_guessed           =0;
             game                    = false;
             message.innerHTML       = "you lose!  press spacebar to reset";
-
+            
         }
 
         function win_check()
@@ -121,26 +143,27 @@
         }
 
         function win()
-        {
-            ws="";
+        {   clear_data();
+            showlink();
             guessed_letters.length  = 0;
             boolio.length           = 0;
-            new_word();
-            update_wordspace();
-            clear_data();
             wins++;
-            //winsd.innerHTML         = "number of wins: " + wins;
             guesses_remaining       =  12;
             times_guessed           = 0;
             game                    = false;
             message.innerHTML       = "you win!  press space bar to restart";
             hangman_pic.style.marginTop = "200px";
             hangman_pic.src         = "assets/images/hangman.gif"
+            wordspace.innerHTML     = ws;
+             new_word();
+             update_wordspace();
+             
         }
 
 
 
         document.onkeyup = function(event) {
+            
             guess = event.key;
             let d = false;
             gen_purpose_var = false;
@@ -174,14 +197,18 @@
                 if(guess==" "){
                     message.innerHTML = "";
                     update_data();
-                    game=true;}
-
+                    game=true;
+                    hidelink();
+                }
+                    
             }
         }
         gen_purpose_var = false;
         good_bad_ugly.play();
         
         };
+        console.log(wordset.length);
+        console.log(links.length);
         gptext.innerHTML = "guessed letters: ";
         winsd.innerHTML = "number of wins: " + wins;
         lossesd.innerHTML = "number of losses: " + losses;
@@ -189,4 +216,3 @@
         guessed.innerHTML = guessed_letters; 
         wordspace.innerHTML = "press space to play";
         game=false;
-        //update
